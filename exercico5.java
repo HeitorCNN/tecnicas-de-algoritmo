9b90c1ccf429350c77c7cca3d9b2ee734c964e8c
HeitorCNN <heitorcaetano2302@gmail.com>
2025-11-23 19:29:29 -0300

    Implement input and divisor calculation for array
    Added functionality to read integers and print their divisors.

--- exercico5.java
+++ exercico5.java
@@ -10,8 +10,26 @@ public class exercico5 {
 		final int TAM = 10;
 		int a [], i,num;
 		
-		a = new int [TAM];
-		
-	}
+		a = new int [TAM]; 
+		 Scanner in = new Scanner(System.in);
 
+        for (int i = 0; i < TAM; i++) {
+            System.out.print("A[" + i + "]: ");
+            a[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < TAM; i++) {
+            System.out.print("Divisores de " + a[i] + ": ");
+
+            for (int d = 1; d <= a[i]; d++) {
+                if (a[i] % d == 0) System.out.print(d + " ");
+            }
+            System.out.println();
+        }
+
+       
+    }
 }
+
+		
+	
